2d9ee6398841c494b5d212e52b39e61b294d4123
Aleksandr Osipov <xundero@mail.ru>
2019-06-30 15:15:46 +0500

    Add service

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-cd ~
+cd /home/appuser/
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
-puma -d
-ps aux | grep puma
+mkdir /home/appuser/reddit
+cd /home/appuser/
+git clone https://github.com/xUndero/reddit_scripts.git
+
